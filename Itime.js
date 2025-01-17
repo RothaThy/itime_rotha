6e2204044deff89b6cdf9da14f388fabab25ca68
RothaThy <165135628+RothaThy@users.noreply.github.com>
2025-01-17 16:17:55 +0700

    Add files via upload

---
+++ Itime.js
@@ -0,0 +1,9 @@
+var showItems = document.getElementById("show1");
+function show(){
+    showItems.style.display="block";
+    showItems.style.transition="all 5s";
+}
+function concel(){
+    showItems.style.display="none";
+    showItems.style.transition="all 5s";
+}
